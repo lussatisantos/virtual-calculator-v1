1f0e82aab963fdfe0095b3e072a267eaca0b6291
Lussati Santos <lussatisantos10@gmail.com>
2023-07-26 14:15:55 +0100

    division act

--- calc_model/__init__.py
+++ calc_model/__init__.py
@@ -13,7 +13,8 @@ def multiplication():
     print(f'The numbers entered are {num} and their multiplication is equal to {tot}')
 
 def division():
-    n1 = float(input('Numerator: '))
-    n2 = float(input('Denominator: '))
-    tot = n1 / n2
-    print(f'The {n1} division to {n2} is equal to {tot}')
+    while True:
+        n1 = float(input('Numerator: '))
+        n2 = float(input('Denominator: '))
+        tot = n1 / n2
+        print(f'The {n1} division to {n2} is equal to {tot}')
